ef2bb324540dbf6d152c1a20de9681697617dbf8
dinesh-ddr <drdmation@gmail.com>
2025-12-18 22:46:29 +0530

    Add VPC and EKS outputs to outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,27 @@
-output "cluster_id" {
-  value = module.eks.cluster_id
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "ecr_repo_url" {
-  value = aws_ecr_repository.repo.repository_url
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "repository_urls" {
+  value = module.ecr.repository_urls
 }
