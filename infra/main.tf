77a03e82558c4f44d7816a351312857864f6de87
dinesh-ddr <drdmation@gmail.com>
2025-12-18 18:37:56 +0530

    Update EKS module configuration and subnet tags

--- infra/main.tf
+++ infra/main.tf
@@ -12,29 +12,44 @@ module "vpc" {
   private_subnets = ["10.0.11.0/24","10.0.12.0/24"]
  enable_nat_gateway = true
   single_nat_gateway = true
+public_subnet_tags = {
+  "kubernetes.io/role/elb" = "1"
+  "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+}
+
+private_subnet_tags = {
+  "kubernetes.io/role/internal-elb" = "1"
+  "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+}
+
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.10.1"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  # v21 uses "name" instead of cluster_name
+  name               = var.cluster_name
+  kubernetes_version = "1.27"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true
+  # helpful defaults
+  endpoint_public_access                    = true
+  enable_cluster_creator_admin_permissions  = true
 
+  # v21 uses eks_managed_node_groups instead of node_groups
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
       min_size       = 1
       max_size       = 3
       desired_size   = 2
     }
   }
 }
+
 
 
 resource "aws_ecr_repository" "repo" {
