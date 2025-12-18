8b0a351b90ccff1a1637aea353284bd99bf7ba03
dinesh-ddr <drdmation@gmail.com>
2025-12-18 22:53:11 +0530

    Create main.tf

---
+++ infra/modules/eks/main.tf
@@ -0,0 +1,186 @@
+########################################
+# IAM: Cluster role
+########################################
+data "aws_iam_policy_document" "cluster_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "cluster_role" {
+  name               = "${var.project_name}-eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.cluster_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSVPCResourceController" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+########################################
+# IAM: Node role
+########################################
+data "aws_iam_policy_document" "node_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "node_role" {
+  name               = "${var.project_name}-eks-node-role"
+  assume_role_policy = data.aws_iam_policy_document.node_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+########################################
+# Security Groups (control plane + worker)
+########################################
+resource "aws_security_group" "control_plane" {
+  name        = "${var.project_name}-control-plane-sg"
+  description = "EKS control plane security group"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_security_group" "worker" {
+  name        = "${var.project_name}-worker-sg"
+  description = "EKS worker node security group"
+  vpc_id      = var.vpc_id
+}
+
+# Worker-to-worker all traffic (needed for Kubernetes networking)
+resource "aws_security_group_rule" "worker_ingress_all_self" {
+  type                     = "ingress"
+  protocol                 = "-1"
+  from_port                = 0
+  to_port                  = 0
+  source_security_group_id = aws_security_group.worker.id
+  security_group_id        = aws_security_group.worker.id
+}
+
+# Control plane -> workers (kubelet + https)
+resource "aws_security_group_rule" "worker_ingress_kubelet_from_cp" {
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 10250
+  to_port                  = 10250
+  source_security_group_id = aws_security_group.control_plane.id
+  security_group_id        = aws_security_group.worker.id
+}
+
+resource "aws_security_group_rule" "worker_ingress_https_from_cp" {
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  source_security_group_id = aws_security_group.control_plane.id
+  security_group_id        = aws_security_group.worker.id
+}
+
+resource "aws_security_group_rule" "worker_egress_all" {
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.worker.id
+}
+
+resource "aws_security_group_rule" "cp_egress_all" {
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.control_plane.id
+}
+
+########################################
+# EKS Cluster
+########################################
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  version  = var.eks_version
+  role_arn = aws_iam_role.cluster_role.arn
+
+  vpc_config {
+    subnet_ids              = var.private_subnet_ids
+    security_group_ids      = [aws_security_group.control_plane.id, aws_security_group.worker.id]
+    endpoint_public_access  = true
+    endpoint_private_access = false
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.cluster_AmazonEKSVPCResourceController
+  ]
+}
+
+########################################
+# Node Group (Managed)
+########################################
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "default"  # keep it simple for screenshots
+  node_role_arn   = aws_iam_role.node_role.arn
+
+  subnet_ids = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = var.node_desired
+    max_size     = var.node_max
+    min_size     = var.node_min
+  }
+
+  instance_types = [var.node_instance_type]
+  disk_size      = var.node_disk_size
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy
+  ]
+}
+
+########################################
+# OIDC Provider (IRSA)
+########################################
+data "tls_certificate" "oidc_thumbprint" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "this" {
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
+}
