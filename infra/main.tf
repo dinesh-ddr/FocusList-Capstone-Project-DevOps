e46464378e8808269707be86ad2530d7a11ae401
dinesh-ddr <drdmation@gmail.com>
2025-12-18 19:02:14 +0530

    Upgrade Kubernetes version to 1.29

--- infra/main.tf
+++ infra/main.tf
@@ -30,7 +30,7 @@ module "eks" {
 
   # v21 uses "name" instead of cluster_name
   name               = var.cluster_name
-  kubernetes_version = "1.27"
+  kubernetes_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
