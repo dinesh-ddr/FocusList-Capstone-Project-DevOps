e3e5e6e21d4c9aeb116a7958e2c7e12832211a7d
dinesh-ddr <drdmation@gmail.com>
2025-12-18 18:31:18 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -10,6 +10,8 @@ module "vpc" {
   azs             = ["ap-south-1a","ap-south-1b"]
   public_subnets  = ["10.0.1.0/24","10.0.2.0/24"]
   private_subnets = ["10.0.11.0/24","10.0.12.0/24"]
+ enable_nat_gateway = true
+  single_nat_gateway = true
 }
 
 module "eks" {
