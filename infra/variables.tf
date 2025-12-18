f301eb6ef425e98a5917720847263cce58d82625
dinesh-ddr <drdmation@gmail.com>
2025-12-18 15:10:23 +0530

    Add Terraform variables for region and cluster

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  default = "focuslist-eks-cluster"
+}
+
+variable "ecr_repo" {
+  default = "focuslist-capstone-repo"
+}
