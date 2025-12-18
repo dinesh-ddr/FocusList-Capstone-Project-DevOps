644cf9700c3a1f8d838b9ac820359f010eb154d8
dinesh-ddr <drdmation@gmail.com>
2025-12-18 22:10:06 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -37,7 +37,7 @@ module "eks" {
   version = "21.10.1"
 
   name               = var.cluster_name
-  kubernetes_version = "1.29"
+  kubernetes_version = "1.30"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
