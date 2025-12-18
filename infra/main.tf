0f1c4106bc804fb65346d19c524f4eaa6807ef92
dinesh-ddr <drdmation@gmail.com>
2025-12-18 19:26:19 +0530

    Add service-linked role for EKS and update VPC config
    Added IAM service-linked role for EKS nodegroups and adjusted subnet tags.

--- infra/main.tf
+++ infra/main.tf
@@ -2,44 +2,53 @@ provider "aws" {
   region = var.region
 }
 
+# Required for EKS Managed Node Groups.
+# Fixes: "AccessDenied: Amazon EKS Nodegroups was unable to assume the service-linked role"
+resource "aws_iam_service_linked_role" "eks_nodegroup" {
+  aws_service_name = "eks-nodegroup.amazonaws.com"
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "6.5.1"
+
   name = "focuslist-vpc"
   cidr = "10.0.0.0/16"
-  azs             = ["ap-south-1a","ap-south-1b"]
-  public_subnets  = ["10.0.1.0/24","10.0.2.0/24"]
-  private_subnets = ["10.0.11.0/24","10.0.12.0/24"]
- enable_nat_gateway = true
-  single_nat_gateway = true
-public_subnet_tags = {
-  "kubernetes.io/role/elb" = "1"
-  "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-}
 
-private_subnet_tags = {
-  "kubernetes.io/role/internal-elb" = "1"
-  "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-}
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
 
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                 = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"        = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.10.1"
 
-  # v21 uses "name" instead of cluster_name
+  # Ensure the service-linked role exists before nodegroups are created
+  depends_on = [aws_iam_service_linked_role.eks_nodegroup]
+
   name               = var.cluster_name
   kubernetes_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # helpful defaults
-  endpoint_public_access                    = true
-  enable_cluster_creator_admin_permissions  = true
+  endpoint_public_access                   = true
+  enable_cluster_creator_admin_permissions = true
 
-  # v21 uses eks_managed_node_groups instead of node_groups
   eks_managed_node_groups = {
     default = {
       instance_types = ["t3.micro"]
@@ -50,10 +59,9 @@ module "eks" {
   }
 }
 
-
-
 resource "aws_ecr_repository" "repo" {
-  name                 = var.ecr_repo
+  name = var.ecr_repo
+
   image_scanning_configuration {
     scan_on_push = true
   }
